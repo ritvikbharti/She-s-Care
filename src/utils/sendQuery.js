7baedfb691418a71eea7ebb5b1a1b597e26451f9
Sourav Kumar <sk4118251@gmail.com>
2024-11-25 23:17:05 +0530

    test chat 2

--- src/utils/sendQuery.js
+++ src/utils/sendQuery.js
@@ -3,11 +3,11 @@ const apikey = import.meta.env.VITE_CHAT_API_KEY;
 export const sendQuery = async(userQuery)=>{
     const options = {
     method: 'POST',
-    url: 'https://api.on-demand.io/chat/v1/sessions/674468213ccc9c388cc850b1/query',
+    url: 'https://api.on-demand.io/chat/v1/sessions/6744b768d2b01e61b6088012/query',
     headers: {
         accept: 'application/json',
         'content-type': 'application/json',
-        apikey: '8vvEKkMf3kEneMTyJ48D7qK70UOeBXtw'
+        apikey: "ELzMUkKxltsHqDmVQLbHCBz1u4Zjdqt5"
     },
     data: {
         responseMode: 'sync',
@@ -18,9 +18,23 @@ export const sendQuery = async(userQuery)=>{
     try{
         const res = await axios.request(options);
         console.log(res.data);
-        console.log(apikey);
         return res.data;
     }catch(error){
         console.log("CANT GET CHAT RESPONSE", error);
     }
 }
+
+
+// {
+//     "message": "Chat session created successfully",
+//     "data": {
+//       "id": "6744b768d2b01e61b6088012",
+//       "companyId": "66e2e025f068cd8a4290863b",
+//       "externalUserId": "sourav2",
+//       "pluginIds": [],
+//       "title": "",
+//       "createdBy": "66e2e025f068cd8a4290863c",
+//       "createdAt": "2024-11-25T17:44:08.40630665Z",
+//       "updatedAt": "2024-11-25T17:44:08.406306695Z"
+//     }
+//   }
